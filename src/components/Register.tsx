1b0b5287d81ac09aa663ce2d7806779a7efc212e
KaiqueMCR <kaiquedev404@gmail.com>
2022-11-29 17:19:56 -0300

    add loading

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { CircleNotch } from 'phosphor-react'
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
 
@@ -7,6 +8,7 @@ export function Register() {
   const [name, setName] = useState('')
   const [tel, setTel] = useState('')
   const [pro, setPro] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const data = {
     name,
@@ -27,14 +29,17 @@ export function Register() {
   async function handleSubmit(e: React.MouseEvent) {
     if (name != '' && email != '' && tel != '' && pro != '' && tel.length == 15) {
       e.preventDefault()
+      setLoading(true)
 
-      try {
-        await axios.post(import.meta.env.VITE_SERVER_URL, data)
-        toast.success('Enviado com sucesso!')
-        setTimeout(() => {
-          window.location.replace(import.meta.env.VITE_REDIRECT_TO)
-        }, 3000)
+        try {
+          await axios.post(import.meta.env.VITE_SERVER_URL, data)
+          toast.success('Enviado com sucesso!')
+          setTimeout(() => {
+            window.location.replace(import.meta.env.VITE_REDIRECT_TO)
+          }, 3000)
+          setLoading(false)
         } catch (error) {
+          setLoading(false)
           toast.error('Ago deu errado. Tente Novamente!')
         }   
       }
@@ -49,7 +54,7 @@ export function Register() {
                 <input required onChange={(e)=> setEmail(e.currentTarget.value)} type="email" placeholder="Email*" className="w-full p-2 rounded outline-[#1c60ad] bg-zinc-200 py-4 placeholder-zinc-500" />
                 <input required onChange={(e)=> setPro(e.currentTarget.value)} type="text" placeholder="Profissão*" className="w-full p-2 rounded outline-[#1c60ad] bg-zinc-200 py-4 placeholder-zinc-500" />
                 <input minLength={16} maxLength={16} placeholder="Celular* (99) 99999-9999" value={tel} onChange={(e) => setTel(maskPhone(e.currentTarget.value))} type="tel" className="w-full p-2 rounded outline-[#1c60ad] bg-zinc-200 py-4 placeholder-zinc-500" />
-                <button onClick={handleSubmit} className="w-full bg-[#005BA6] px-8 py-4 text-white rounded hover:bg-[#005BA6] transition-colors">Enviar</button>
+                <button disabled={loading} onClick={handleSubmit} className="w-full flex justify-center items-center text-center bg-[#005BA6] px-8 py-4 text-white rounded hover:bg-[#005BA6] transition-colors">{loading ? <CircleNotch size={24} className="text-white animate-spin" />: "Enviar" }</button>
             </form>
             <p className="text-white">powered by <a className="hover:underline" href="https://payjob.com.br">www.payjob.com.br</a></p>
         </div>
